38e5acad535bc8a97ceb129560b68288c63461a8
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-06-10 16:21:36 +0200

    add v-for for singlecard with function api

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,7 +1,11 @@
 <script>
 import axios from "axios" ;
+import SingleProject from "./SingleProject.vue";
 export default {
     name: 'ProjectList',
+    components:{
+        SingleProject
+    },
     data() {
         return {
             projects: []
@@ -25,18 +29,11 @@ export default {
     <div class="container">
         <h1 class="text-center">Lista Progetti</h1>
         <div class="row row-cols-3 gy-3">
-            <div class="col">
-                <div class="card">
-                    <!-- <img src="..." class="card-img-top" alt="..."> -->
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
-            </div>
+           <SingleProject v-for="project in projects" :key="project.id" :projectInfo="project" ></SingleProject>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SingleProject.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'SingleProject',
+    props:{
+        projectInfo:Object
+    }
+}
+</script>
+
+<template>
+            <div class="col" >
+                <div class="card" >
+                    <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div class="card-body text-center">
+                        <h5 class="card-title">
+                            <strong>Name:</strong> {{  projectInfo.name }}
+                        </h5>
+                        <h5 class="card-title">
+                            <strong>Client_Name:</strong>{{  projectInfo.client_name }}
+                        </h5>
+                        <p v-show="projectInfo.summary" class="card-text">
+                            <strong>Content:</strong>  {{  projectInfo.summary }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+</template>
+
+<style lang="scss" scoped>
+.card{
+    min-height: 400px;
+}
+</style>
