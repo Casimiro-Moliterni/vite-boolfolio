37395a9714a2952f2bf9019f6cf1ac9273730653
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-06-10 16:33:57 +0200

    add technlogies and type for singleproject

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -18,7 +18,16 @@ export default {
                         <h5 class="card-title">
                             <strong>Client_Name:</strong>{{  projectInfo.client_name }}
                         </h5>
-                        <p v-show="projectInfo.summary" class="card-text">
+                        <div v-if=" projectInfo.type " class="text-start">
+                            <strong>Type:</strong>{{ projectInfo.type.name }}
+                        </div>
+                        <div v-if="projectInfo.technologies.length > 0" class="text-start">
+                            <strong>Technologies:</strong>
+                            <span v-for="technology in projectInfo.technologies">
+                                {{  technology.name}} &nbsp;
+                            </span>
+                        </div>
+                        <p v-show="projectInfo.summary" class="card-text text-start">
                             <strong>Content:</strong>  {{  projectInfo.summary }}
                         </p>
                     </div>
